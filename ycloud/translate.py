34136c7dbbe83e2f9eadc26244694008f2904871
chessnok <mikhail.fadin@outlook.com>
2024-06-01 21:54:19 +0200

    Added html translation support

--- ycloud/translate.py
+++ ycloud/translate.py
@@ -7,7 +7,7 @@ class Translate:
     def __init__(self, service_account_file):
         self.yc = YandexCloud(service_account_file)
 
-    def translate(self, text, lang):
+    def translate(self, text, lang, is_html=False, source_lang="en"):
         url = "https://translate.api.cloud.yandex.net/translate/v2/translate"
         headers = {
             'Content-Type': 'application/json',
@@ -16,7 +16,9 @@ class Translate:
         data = {
             'folder_id': self.yc.data['folder_id'],
             'texts': [text],
-            'targetLanguageCode': lang
+            'targetLanguageCode': lang,
+            'sourceLanguageCode': source_lang,
+            'format': 'PLAIN_TEXT' if not is_html else 'HTML'
         }
         response = requests.post(url, headers=headers, json=data)
         response.raise_for_status()
@@ -25,5 +27,4 @@ class Translate:
 
 if __name__ == "__main__":
     t = Translate("service_account.json")
-    print(t.translate("Hello, world!", "ru"))
-    print(t.translate("Привет, мир!", "en"))
+    print(t.translate("<p><a href=\"google.com\">Hello, Google</a>", "ru", source_lang="en", is_html=True))
